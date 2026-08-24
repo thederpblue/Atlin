d74c69830949684410fd8db0e0308b37e042900b
MalteFP <maltefp@outlook.com>
2026-08-24 21:38:21 +0200

    added spikes

---
+++ 2d-platformer/spike.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+func _on_spike_hit_box_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.name == "player":
+		body.position = body.respawnPoint

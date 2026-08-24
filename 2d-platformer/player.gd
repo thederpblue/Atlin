de6162af3d80254a9265fd4dd06f88d92feb5d7f
MalteFP <maltefp@outlook.com>
2026-08-24 20:42:59 +0200

    Fixed most movement

--- 2d-platformer/player.gd
+++ 2d-platformer/player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody2D
 
+var was_left = false
+var was_right = false
+
+
+var velocityQueue = []
+var velocityBlock = 0
+
+
 var dashes = 1
 var timeSinceDash = 0
 var timeOnFloor = 0
@@ -29,67 +37,89 @@ func _physics_process(delta):
 	get_node("Label").text = str(velocity)
 	updateTimes(delta)
 	updateIcon()
+	applyfriction(delta)
+	applyGravity(delta)
+	updateDashes(delta)
+	updateVelocityQueue(delta)
 	
-		
-
-	#FRICTION
-	
-	
-	#GRAVITY
-	if (timeSinceDash > 0.25 and jumpTime == 0 || jumpTime > maxJumpTime):
-		if velocity.y + gravity * delta > fallMaxSpeed:
-			velocity.y = fallMaxSpeed
-		else:
-			velocity.y += delta * gravity
-	
-	#UPDATE DASHES
-	if is_on_floor():
-		dashes = 1
-	
-	if timeSinceDash < dashTime and !timeSinceDash == 0:
-		velocity = dashDir * dashVelocity
-	if timeSinceDash > dashTime and timeSinceDash - delta < dashTime:
-		velocity = dashDir * afterDashVelocity
 	
 	
 	
 	#DETECT MOVEMENT
+	var left = Input.is_key_pressed(KEY_LEFT)
+	var right = Input.is_key_pressed(KEY_RIGHT)
+	
 	jumpTime += delta
 	if Input.is_key_pressed(KEY_SPACE):
 		jump(delta)
 	elif is_on_floor():
 		jumpTime = 0
-	if Input.is_key_pressed(KEY_LEFT):
+	if left:
 		walk_left(delta)
-	if Input.is_key_pressed(KEY_RIGHT):
+	if right:
 		walk_right(delta)
-	if !Input.is_key_pressed(KEY_LEFT) and !Input.is_key_pressed(KEY_RIGHT):
+	if (was_left || was_right) and !left and !right:
 		stop_walk()
 	if Input.is_key_pressed(KEY_C) and dashes >  0 and timeSinceDash > dashCooldown:
-		dash()
+		dash(delta)
 	
-	
+	was_left = left
+	was_right = right
 	
 	# APPLY MOVEMENT
 	move_and_slide()
 	
-func friction(delta):
-	if abs(velocity.x) > maxWalkVelocity:
-		velocity.x -= delta * decelerationAboveMax
+func updateVelocityQueue(delta):
+	for i in range(velocityQueue.size() - 1, -1, -1):
+		
+		if velocityQueue[i].constant:
+			if velocityQueue[i].add:
+				velocity += velocityQueue[i].velocity * delta
+			else:
+				velocity = velocityQueue[i].velocity
+		elif velocityQueue[i].time - delta <= 0:
+			if velocityQueue[i].add:
+				velocity += velocityQueue[i].velocity
+			else:
+				velocity = velocityQueue[i].velocity
+		velocityQueue[i].time -= delta
+		if velocityQueue[i].time <= 0:
+			velocityQueue.pop_at(i)
+
+func addToVeloctiyQueue(velocity: Vector2, time: float, constant: bool, add: bool):
+	velocityQueue.push_back(QueueVelocity.new(velocity, time, constant, add)) 
 	
+	
+func updateDashes(delta):
+	if is_on_floor() and velocityBlock <= 0 - 0.01:
+		dashes = 1
+	
+	
+func applyGravity(delta):
+	if velocityBlock <= 0:
+		velocity.y += gravity * delta
+
+func applyfriction(delta):
+	if velocityBlock <= 0 and is_on_floor():
+		if velocity.x > 0:
+			velocity.x -= delta * decelerationAboveMax
+		elif velocity.x < 0:
+			velocity.x += delta * decelerationAboveMax
 	
 func updateIcon():
+	if velocity.x != 0:
+		get_node("AnimatedSprite2D").play("Walking")
+	else:
+		get_node("AnimatedSprite2D").stop()
+		get_node("AnimatedSprite2D").play("Idle")
 	if timeSinceDash > 0.25:
 		get_node("GPUParticles2D").emitting = false
-		if dashes == 0:
-			get_node("Sprite2D").texture = load("res://theoWalk.png")
-		elif dashes == 1:
-			get_node("Sprite2D").texture = load("res://theoWalk.png")
 	else:
 		get_node("GPUParticles2D").emitting = true
-		get_node("Sprite2D").texture = load("res://theoWalk.png")
+	
 
 func updateTimes(delta):
+	velocityBlock -= delta
 	timeSinceDash += delta
 	if is_on_floor():
 		timeOnFloor += delta
@@ -105,24 +135,31 @@ func jump(delta):
 
 func walk_left(delta):
 	get_child(0).scale.x = -1
-	if velocity.x - (walkAcceleration * delta) < -maxWalkVelocity:
-		velocity.x = -maxWalkVelocity
-	else:
-		velocity.x -= walkAcceleration * delta
+	if velocityBlock <= 0:
+		if velocity.x > -maxWalkVelocity:
+			if velocity.x - (walkAcceleration * delta) < -maxWalkVelocity:
+				velocity.x = -maxWalkVelocity
+			elif velocity.x > -maxWalkVelocity:
+				velocity.x -= walkAcceleration * delta
 
 func walk_right(delta):
 	get_child(0).scale.x = 1
-	if velocity.x + (walkAcceleration * delta) > maxWalkVelocity:
-		velocity.x = maxWalkVelocity
-	else:
-		velocity.x += walkAcceleration * delta
+	if velocityBlock <= 0:
+		if velocity.x < maxWalkVelocity:
+			if velocity.x + (walkAcceleration * delta) > maxWalkVelocity:
+				velocity.x = maxWalkVelocity
+			else:
+				velocity.x += walkAcceleration * delta
 	
 func stop_walk():
 	velocity.x = 0
 
-func dash():
+func dash(delta):
 	dashDir = get_dir().normalized()
 	print(dashDir)
+	addToVeloctiyQueue(dashDir * dashVelocity, dashTime, true, false)
+	addToVeloctiyQueue(dashDir * afterDashVelocity, dashTime + delta, false, false)
+	velocityBlock = 0.25
 	dashes -= 1
 	timeOnFloor = -0.25
 	timeSinceDash = 0

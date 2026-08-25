b45c8aad958bdef3c4008029d056c5030fab3990
MalteFP <maltefp@outlook.com>
2026-08-25 21:05:39 +0200

    Readded missing files

---
+++ 2d-platformer/exit.gd
@@ -0,0 +1,8 @@
+extends Node2D
+var carry_velocity := Vector2.ZERO
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.name == "player":
+		ScreenHolder.carry_velocity = body.velocity
+		get_tree().change_scene_to_file(ScreenHolder.screenList[ScreenHolder.currentScreen + 1])
---
+++ 2d-platformer/screen_holder.gd
@@ -0,0 +1,4 @@
+extends Node
+var screenList = ["res://screens/level_one.tscn", "res://screens/level_two.tscn"]
+var currentScreen = 0
+var carry_velocity = Vector2.ZERO

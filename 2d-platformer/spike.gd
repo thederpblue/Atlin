2beefaba398f6636297dc84c80254cd44a9213f3
MalteFP <MalteFp@outlook.com>
2026-08-26 08:21:50 +0200

    Removed spike scene as spike tilemap works better

--- 2d-platformer/spike.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-
-func _on_spike_hit_box_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	if body.name == "player":
-		body.velocity = Vector2.ZERO
-		body.position = body.respawnPoint
-		body.velocityQueue = []
